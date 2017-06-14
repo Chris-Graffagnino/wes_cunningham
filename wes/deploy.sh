31d468df8a9c6a3c4c7126c17c87f97845bc4ea4
Chris Graffagnino <nashguitar1@gmail.com>
2017-06-13 22:45:27 -0500

    Edit deploy script

--- wes/deploy.sh
+++ wes/deploy.sh
@@ -16,11 +16,11 @@ if [[ $(git status -s) ]]
 then
     echo "Adding and committing everything in develop branch"
     git add . && git commit -m "`date`"
-    git push origin develop 
+    git push origin develop
 fi
 
 echo "Checking out master branch into public"
-git worktree add -B master public origin/master
+git worktree add -B gh-pages public origin/master
 
 echo "Removing existing files"
 rm -rf public/*
@@ -32,4 +32,4 @@ echo "Updating master branch"
 cd public && git add --all && git commit -m "Publishing to github (deploy.sh)"
 
 echo "Pushing changes"
-git push origin master -f
+git push origin gh-pages -f
